07659d6a79112b108febda95bbb3a6f095071efe
Peter <mateuspeternelli@gmail.com>
2022-12-13 17:31:19 -0300

    adicionar post get e put

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const tarefas_controller = require('./tarefas-controller')
 
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb+srv://mateus:1234@cluster0.0qf4e9p.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true})
+.then(() => console.log("Conexao realizada com sucesso"))
 mongoose.Promise = global.Promise
 try {
     let db = mongoose.connection
-    db.on('errr', console.error.bind(console, 'erro de conexao no banco'))
+    db.on('errr', console.error.bind(console, 'Erro de conexao no banco'))
 } catch (e) {
     console.log(e)
 }
@@ -24,9 +25,13 @@ app.get('/', (req, res) =>{
     res.send('teste oi')
 })
 
-router.post('/tarefas', tarefas_controller.cadastrarTarefa)
-router.get('/tarefas', tarefas_controller.listarTarefas)
-router.get('/tarefas/:id', tarefas_controller.buscarTarefa)
+router.post('/tarefas', tarefas_controller.cadastrarTarefas)
+router.get('/tarefas', tarefas_controller.listarTarefa)
+router.get('/tarefas:id', tarefas_controller.buscarTarefa)
+router.delete('/tarefas:id', tarefas_controller.deletarTarefa)
+router.put('/tarefas:id', tarefas_controller.editarTarefa)
+
+app.use('/', router)
 
 let porta = process.env.PORT || 5000
 
--- tarefas-controller.js
+++ tarefas-controller.js
@@ -3,7 +3,7 @@ var Tarefas = require('./tarefas-model')
 exports.listarTarefa = function (req, res) {
     Tarefas.find({}, function (err, tarefas) {
         if (err) return next(err)
-        return res.json(estudantes);
+        return res.json(tarefas);
     })
 }
 
@@ -14,17 +14,31 @@ exports.buscarTarefa = function (req, res) {
     })
 }
 
+exports.deletarTarefa = function (req, res){
+    Tarefas.deleteOne({_id:req.params.id}, function(err, tarefa){
+        if (err) console.log("Tarefa não editada"), next(err)
+        return console.log("Tarefa deletada com sucesso"), res.send("Tarefa deletada com sucesso")
+    })
+}
+
+exports.editarTarefa = function (req, res){
+    Tarefas.updateOne({_id:req.params.id}, req.body, function(err, tarefa){
+        if (err) return console.log("Tarefa não editada")
+        return console.log("Tarefa editada com sucesso"), res.send("Tarefa editada com sucesso")
+    })
+    
+}
 
 exports.cadastrarTarefas = function (req, res){
-    let Tarefas = new Tarefas({
+    let tarefa = new Tarefas({
         descricao: req.body.descricao,
         prazo: req.body.prazo,
         completa: req.body.completa,
     })
-    Tarefas.save(function(errr){
+    tarefa.save(function(err){
         if(err){
             return next(err)
         }
     })
-    res.send("tarefa cadastrada com sucesso")
+    return console.log("Tarefa cadastrada com sucesso"), res.send("tarefa cadastrada com sucesso")
 }
